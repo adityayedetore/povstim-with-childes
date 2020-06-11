3e3d52f9a335ae7702961a7cf08250bc97539c71
Aditya Yedetore <aditya.yedetore@gmail.com>
2020-06-11 06:54:48 -0500

    now produces gloss-and-filename.txt

--- data/tsv-to-utterances.R
+++ data/tsv-to-utterances.R
@@ -1,31 +1,35 @@
 #######################################################################
 ## Script to collect all the utterances from the files produced 
-## by calling collect-data.R. 
+## by calling collect-data.R
 ##
-## The only processing done here is to remove the utterances of the 
-## child, and to remove the utterances that have no content (e.g. 
-## "." utterances)
+## Removes CHI utterances and utterances with no content (e.g. "."
+## utterances)
 ##
-## The utterances are output to output.txt
+## Produces:
+##   output.txt - the utterances only
+##   gloss-and-filename.txt - utterances and filename
+##
+## Overwrites previous files. 
 #######################################################################
 
 library(stringr)
 
-get_utterances <- function(f) {
-    somedata <- read.table(file=f, header=TRUE)
-    return(somedata[,c("Speaker", "Gloss")])
-}
-
+print("====gathering data====")
+file.create("gloss-and-filename.txt")
 file.create("output.txt")
 paths = list.files("processed", full.names=TRUE)
-output = c()
 for (p in paths) {
     print(p)
-    utts =  get_utterances(p)
+    utts<-read.table(file=p, header=TRUE)
+    utts<-utts[,c("Speaker", "Gloss", "Filename")]
     utts<-utts[!(utts$Speaker=="CHI"),]
     utts<-utts[!(str_length(utts$Speaker)>5),]
-    utts<-utts[!(str_length(utts$Gloss)==1),]
-    utts$Speaker = as.character(utts$Speaker)
+    utts<-utts[!(str_length(utts$Gloss)<=1),]
+    w = utts[,c("Gloss", "Filename")]
+    write.table(w, file="gloss-and-filename.txt", sep="\t",
+                col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE)
     write.table(utts$Gloss, file="output.txt", sep="\t", 
                 col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE)
 }
+print("produced gloss-and-filename.txt")
+print("produced output.txt")
