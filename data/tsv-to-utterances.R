70854b260f1e4df96b76bc076047cb477961024a
Aditya Yedetore <aditya.yedetore@gmail.com>
2020-07-01 11:46:06 -0500

    Small changes

--- data/tsv-to-utterances.R
+++ data/tsv-to-utterances.R
@@ -6,7 +6,7 @@
 ## utterances)
 ##
 ## Produces:
-##   output.txt - the utterances only
+##   all-utterances.txt - all the utterances
 ##   gloss-and-filename.txt - utterances and filename
 ##
 ## Overwrites previous files. 
@@ -23,20 +23,21 @@ is_child <- function(row) {
 
 print("====gathering data====")
 file.create("gloss-and-filename.txt")
-file.create("output.txt")
+file.create("all-utterances.txt")
 paths = list.files("processed", full.names=TRUE)
 for (p in paths) {
     print(p)
-    utts<-read.table(file=p, header=TRUE)
-    utts<-utts[,c("Speaker", "Gloss", "Filename", "Participants")]
-    utts<-utts[!apply(utts, 1, is_child),]
-    utts<-utts[!(str_length(utts$Speaker)>5),]
+    master<-read.table(file=p, header=TRUE)
+    utts<-master[,c("Speaker", "Gloss", "Filename", "Participants")]
     utts<-utts[!(str_length(utts$Gloss)<=1),]
-    w = utts[,c("Gloss", "Filename")]
-    write.table(w, file="gloss-and-filename.txt", sep="\t",
+    utts<-utts[!apply(utts, 1, is_child),]
+    write.table(utts[,c("Gloss", "Filename")], 
+                file="gloss-and-filename.txt", sep="\t",
                 col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE)
-    write.table(utts$Gloss, file="output.txt", sep="\t", 
-                col.names=FALSE, row.names=FALSE, quote=FALSE, append=TRUE)
+    write.table(master[,c("Gloss", "Filename", "Speaker")], 
+                          file="all-utterances.txt", sep="\t", 
+                          col.names=FALSE, row.names=FALSE, 
+                          quote=FALSE, append=TRUE)
 }
 print("produced gloss-and-filename.txt")
-print("produced output.txt")
+print("produced all-utterances.txt")
