403961e4867d4859c8fa55bc9ea0a0135d98d2fa
Aditya Yedetore <aditya.yedetore@gmail.com>
2020-08-19 16:12:55 -0500

    all gen files

--- gen/make-eval-sets.py
+++ gen/make-eval-sets.py
@@ -10,27 +10,31 @@ def move(sent, keyword="MAIN-AUX"):
     quest.pop(i + 1)
     return ' '.join(decl) + " . " + ' '.join(quest) + " ?"
 
-def gen(): 
-    with open('cfg.txt') as f:
+def ambiguous(sent, keyword="MAIN-AUX"):
+    words = sent.split()
+    i = words.index(keyword) + 1
+    main_aux = words[i]
+    return words.index(main_aux) != i
+
+def gen(cfg_filename, output_filename): 
+    with open(cfg_filename) as f:
         cfg_string = f.read()
-
     grammar = CFG.fromstring(cfg_string)
-
     gen = ""
-    for sentence in generate(grammar):
+    for sentence in generate(grammar, depth=5):
         gen += " ".join(sentence) + "\n"
+    print("====done generating====")
     gen = gen[:-1]
-
     gen = gen.split("\n")
-
-    
     gen_str = ""
     for sent in gen: 
-        gen_str += move(sent) + "\n"
+        if (not ambiguous(sent)):
+            gen_str += move(sent) + "\n"
     gen_str = gen_str[:-1]
+    print("====saving " + output_filename + "====")
 
-    print("====saving decl-quest-gen.txt====")
-
-    with open("gen-set.txt", "w") as f:
+    with open(output_filename, "w") as f:
         f.write(gen_str)
-gen()
+
+gen("test-cfg.txt", "decl-quest-test-set.txt")
+gen("gen-cfg.txt", "decl-quest-gen-set.txt")
