0725336f93fe4027143f06b41f5fa4f4edb60728
Aditya yedetore <aditya.yedetore@gmail.com>
2020-08-24 22:45:13 -0400

    now continuously appends to file

--- gen/make-eval-sets.py
+++ gen/make-eval-sets.py
@@ -1,5 +1,6 @@
 from nltk.parse.generate import generate
 from nltk import CFG
+import os
 
 def move(sent, keyword="MAIN-AUX"):
     decl = sent.split()
@@ -18,6 +19,7 @@ def ambiguous(sent, keyword="MAIN-AUX"):
 
 def gen(cfg, vocab="vocab-cfg.txt", output="output.txt"): 
     print("reading from " + cfg + " and printing to " + output)
+    print("====generating====")
     with open(cfg) as f:
         cfg_string = f.read() + "\n"
     with open (vocab) as f:
@@ -29,21 +31,23 @@ def gen(cfg, vocab="vocab-cfg.txt", output="output.txt"):
         if count % 100000 == 0:
             print(str(count) + " done")
         gen += " ".join(sentence) + "\n"
-    print("====done generating====")
+    print("====moving====")
     gen = gen[:-1]
     gen = gen.split("\n")
     gen_str = ""
     count = 0
-    for sent in gen: 
-        if count % 100000 == 0:
-            print(str(count) + " done")
-        if (not ambiguous(sent)):
-            gen_str += move(sent) + "\n"
-    gen_str = gen_str[:-1]
+    with open(output, "w") as f:
+        for sent in gen: 
+            if count % 100000 == 0:
+                print(str(count) + " done")
+                f.write(gen_str[count:])
+            if (not ambiguous(sent)):
+                gen_str += move(sent) + "\n"
+        gen_str = gen_str[:-1]
     print("====saving to " + output + "====")
 
     with open(output, "w") as f:
-        f.write(gen_str)
+        f.write(gen_str[count:])
 
 gen("test-cfg.txt", vocab="vocab-cfg.txt", output="decl-quest-test-set.txt~medium")
 gen("gen-cfg.txt", vocab="vocab-cfg.txt", output="decl-quest-gen-set.txt~medium")
