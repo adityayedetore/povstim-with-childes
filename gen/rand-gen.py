546f7ca59756f37729d6b7a8b88f842781c06e75
Aditya Yedetore <aditya.yedetore@gmail.com>
2020-08-25 10:41:05 -0500

    now randomly samples from CFG

---
+++ gen/rand-gen.py
@@ -0,0 +1,67 @@
+# Code taken from https://eli.thegreenplace.net/2010/01/28/generating-random-sentences-from-a-context-free-grammar
+# All code in the public domain
+
+from collections import defaultdict
+import random
+
+class CFG(object):
+    def __init__(self):
+        self.prod = defaultdict(list)
+    def add_prod(self, lhs, rhs):
+        prods = rhs.split('|')
+        for prod in prods:
+            self.prod[lhs].append(tuple(prod.split()))
+    def gen_random(self, symbol):
+        sentence = ""
+        rand_prod = random.choice(self.prod[symbol])
+        for sym in rand_prod:
+            if sym in self.prod:
+                sentence += self.gen_random(sym)
+            else:
+                sentence += sym + ' ' 
+        return sentence 
+
+def move(sent, keyword="MAIN-AUX"):
+    decl = sent.split()
+    decl.pop(decl.index(keyword))
+    quest = sent.split()
+    i = quest.index(keyword)
+    quest.insert(0, quest.pop(i + 1))
+    quest.pop(i + 1)
+    return ' '.join(decl) + " . " + ' '.join(quest) + " ?"
+
+def ambiguous(sent, keyword="MAIN-AUX"):
+    words = sent.split()
+    i = words.index(keyword) + 1
+    main_aux = words[i]
+    return words.index(main_aux) != i
+
+def gen(cfg, vocab, output, n):
+    grammar = CFG()
+    print("reading from " + cfg + " and printing to " + output)
+    print("====generating====")
+    with open(cfg, 'r') as f:
+        cfg_rules = f.readlines()
+    for rule in cfg_rules:
+        if rule.strip():
+            lhs = rule.split("->")[0].strip()
+            rhs = rule.split("->")[1]
+            grammar.add_prod(lhs, rhs)
+    with open(vocab, 'r') as f:
+        cfg_rules = f.readlines()
+    for rule in cfg_rules:
+        if rule.strip():
+            lhs = rule.split("->")[0].strip()
+            rhs = rule.split("->")[1]
+            grammar.add_prod(lhs, rhs)
+    with open(output, 'w') as f:
+        while n > 0:
+            sent = grammar.gen_random('S')
+            if not ambiguous(sent):
+                f.write(move(sent) + "\n")
+                n -= 1
+
+
+if __name__ == "__main__":
+    gen(cfg="test-cfg.txt", vocab="vocab-cfg.txt", output="decl-quest-test-set.txt", n=10000)
+    gen(cfg="gen-cfg.txt", vocab="vocab-cfg.txt", output="decl-quest-gen-set.txt", n=10000)
