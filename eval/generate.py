7202a20e056c1579152dc14628010b8a9b89f025
Aditya Yedetore <aditya.yedetore@gmail.com>
2020-08-26 15:04:24 -0500

    now saves to results.txt

--- eval/generate.py
+++ eval/generate.py
@@ -24,6 +24,10 @@ parser.add_argument('--interactive', action='store_true',
         help='word by word interactive mode')
 parser.add_argument('--model', type=str, default="model.pt",
         help="path to model to evaluate on")
+parser.add_argument("--recall_k", type=int, default=10, 
+        help="top k to check")
+parser.add_argument('--results', type=str, default='results.txt',
+        help="path to store eval results")
 parser.add_argument("--cuda", action='store_true',
         help="use cuda")
 args = parser.parse_args()
@@ -68,10 +72,11 @@ def auto_eval(dictionary, hidden, model):
     f = open(args.eval, "r")
     ratings = []
     preds = []
+    count = 0
+    print("recall@" + str(args.recall_k))
     for line in f:
         for word in line.split():
             if (word not in dictionary.word2idx):
-                print(word + " is <unk>")
                 word = "<unk>"
             data = torch.tensor([[dictionary.word2idx[word]]])
             if args.cuda:
@@ -79,12 +84,17 @@ def auto_eval(dictionary, hidden, model):
             output, hidden = model(data, hidden)
             output = output.cpu()
             o = output.numpy()[0][0]
-            idx = np.argpartition(o, -10)[-10:] 
+            idx = np.argpartition(o, -args.recall_k)[-args.recall_k:] 
             idx = idx[np.argsort(o[idx])]
             idx = idx[::-1]
             ratings.append(int(word in preds))
             preds = [dictionary.idx2word[x] for x in idx]
-    print("average: " + str(np.mean(ratings)))
+            count = count + 1
+            if count % 1000 == 0:
+                print(str(np.mean(ratings)))
+    print("final average: " + str(np.mean(ratings)))
+    with open(args.results, 'w') as f:
+        f.write("final average: " + str(np.mean(ratings)))
 
 with open(args.model, 'rb') as f:
     if args.cuda:
